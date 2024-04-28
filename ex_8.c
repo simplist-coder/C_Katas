b13b6d81b1b9d757a980eaa00e56151c4e1a6477
scoder <simplist.coder@gmail.com>
2024-04-28 08:33:57 +0200

    added exercise stubs

---
+++ ex_8.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello Exercise 8!\n");
+    return 0;
+}
---
+++ ex_9.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello Exercise 9!\n");
+    return 0;
+}
